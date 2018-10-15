8b14277b31a06b64979131276bbee7ea7e17ada6
Zack Dunham (RIT Student) <zsd7200@ad.rit.edu>
2018-10-15 10:52:08 -0400

    Final in-class push

---
+++ GDD2UnityGame/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("Play");
+    }
+
+    public void LevelSelect()
+    {
+        Debug.Log("Level Select");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
