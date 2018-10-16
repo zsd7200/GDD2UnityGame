a142f4d56fa8227591ebe4eadafe3ed6080a6281
nat7611 <nat7611@IGM2000-21>
2018-10-15 22:16:45 -0400

    Bare-basic field, generates orbs.

---
+++ GDD2UnityGame/Assets/Scripts/GameScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+    GameObject[] field;
+    [SerializeField] const int width = 7;
+    [SerializeField] const int height = 15;
+    [SerializeField] const float padding = .5f; //Distance between orbs on board
+    const int size = width * height;
+
+	// Use this for initialization
+	void Start () {
+		field = new GameObject[size];
+        for(int i = 0; i < size; i++) //Fills table with random orbs
+        {
+            GameObject orb = Instantiate(Resources.Load("Prefabs/Orb"), new Vector3(), Quaternion.identity) as GameObject;
+            orb.GetComponent<Orb>().type = (OrbType)Random.Range(0, 5); //Will need to change this later because pure random isn't going to get a lot of possible moves
+            orb.transform.position = new Vector3((-(width - 1) / 2f + i % width) * padding, i / width * padding); //Grid is centered horizontally at the position and builds upward
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GDD2UnityGame/Assets/Scripts/Orb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OrbType { Red, Yellow, Green, Blue, Black}
+public class Orb : MonoBehaviour {
+    public OrbType type;
+	// Use this for initialization
+	void Start () {
+        switch (type) //Loads model and color (still need models)
+        {
+            case OrbType.Red:
+                gameObject.GetComponent<MeshRenderer>().materials[0] = Resources.Load("Material/Orb Red") as Material;
+                break;
+            case OrbType.Yellow:
+                gameObject.GetComponent<MeshRenderer>().materials[0] = Resources.Load("Material/Orb Yellow") as Material;
+                break;
+            case OrbType.Green:
+                gameObject.GetComponent<MeshRenderer>().materials[0] = Resources.Load("Material/Orb Green") as Material;
+                break;
+            case OrbType.Blue:
+                gameObject.GetComponent<MeshRenderer>().materials[0] = Resources.Load("Material/Orb Blue") as Material;
+                break;
+            case OrbType.Black:
+                gameObject.GetComponent<MeshRenderer>().materials[0] = Resources.Load("Material/Orb Black") as Material;
+                break;
+        }
+	}
+}
