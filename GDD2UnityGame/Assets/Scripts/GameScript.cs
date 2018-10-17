57e392fccdc55e2811b2f358baf556bfc98590b7
Rob Piwko <rdp6158@rit.edu>
2018-10-17 08:42:44 -0400

    tryin

--- GDD2UnityGame/Assets/Scripts/GameScript.cs
+++ GDD2UnityGame/Assets/Scripts/GameScript.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameScript : MonoBehaviour {
+
+	//public so we can access them in the editor.  easier to change for testing
     GameObject[] field;
-    [SerializeField] const int width = 7;
-    [SerializeField] const int height = 15;
-    [SerializeField] const float padding = .5f; //Distance between orbs on board
-    const int size = width * height;
+    public int width;
+	public int height;
+    public float padding = .5f; //Distance between orbs on board
+	public int startX;
+	public int startY;
+    int size;
+	public GameObject[,] allOrbs;
 
 	// Use this for initialization
 	void Start () {
+		//sets size
+		size = width * height;
+
+		//makes 2d array for orbs
+		allOrbs = new GameObject[width,height];
+
 		field = new GameObject[size];
-        for(int i = 0; i < size; i++) //Fills table with random orbs
-        {
-            GameObject orb = Instantiate(Resources.Load("Prefabs/Orb"), new Vector3(), Quaternion.identity) as GameObject;
-            orb.GetComponent<Orb>().type = (OrbType)Random.Range(0, 5); //Will need to change this later because pure random isn't going to get a lot of possible moves
-            orb.transform.position = new Vector3((-(width - 1) / 2f + i % width) * padding, i / width * padding); //Grid is centered horizontally at the position and builds upward
-        }
+
+		//Fills table with random orbs
+		for(int i = 0; i < width; i++){  //		for(int i = 0 + startX; i < width + startX; i++){
+			for(int j = 0; j < height; j++){ //			for(int j = 0 + startY; j < height + startY; j++){
+				Vector2 tempPos = new Vector2 (i, j);
+				GameObject orb = Instantiate(Resources.Load("Prefabs/Orb"), tempPos, Quaternion.identity) as GameObject;
+	            orb.GetComponent<Orb>().type = (OrbType)Random.Range(0, 5); //Will need to change this later because pure random isn't going to get a lot of possible moves
+	            //orb.transform.position = new Vector3((-(width - 1) / 2f + i % width) * padding, i / width * padding); //Grid is centered horizontally at the position and builds upward
+				orb.name = "( " + i + ", " + j + " )";
+				allOrbs[i,j] = orb;
+			}
+		}
 	}
 	
 	// Update is called once per frame
---
+++ GDD2UnityGame/Assets/Scripts/OrbScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbScript : MonoBehaviour {
+
+	private Vector2 firstTouchPos;
+	private Vector2 finalTouchPos;
+	public float swipeAngle = 0;
+
+	public int row;
+	public int column;
+	private GameScript board;
+	private GameObject otherdot;
+	public int targetX;
+	public int targetY;
+
+	// Use this for initialization
+	void Start () {
+		board = FindObjectOfType<GameScript> ();
+		targetX = (int)transform.position.x;
+		targetY = (int)transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetX = column;
+		targetY = row;
+
+	}
+
+	private void OnMouseDown(){
+		firstTouchPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+	}
+
+	private void OnMouseUp(){
+		finalTouchPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		CalcAngle ();
+	}
+
+	void CalcAngle(){
+		swipeAngle = Mathf.Atan2 (finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x) * 180 / Mathf.PI;
+		Debug.Log (swipeAngle);
+	}
+
+	void MovePieces(){
+		//right swipe
+		if (swipeAngle > -45 && swipeAngle <= 45) {
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					if (j == row) {
+						//if its the last emelment
+						if(board.allOrbs[i +1,j] == null){
+							board.allOrbs [i, j] = board.allOrbs[0, j];
+							continue;
+						}
+						board.allOrbs [i, j] = board.allOrbs[i + 1, j];
+					}
+				}
+			}
+		}
+
+		//left swipe
+	}
+
+
+}
