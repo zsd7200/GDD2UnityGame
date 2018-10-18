6cdee595b2f454b07b74f307019aec662fe90872
zsd7200 <zsd7200@IGM2000-06>
2018-10-18 17:45:51 -0400

    Orbs move to the right (thanks to Nathan)

--- GDD2UnityGame/Assets/Scripts/GameScript.cs
+++ GDD2UnityGame/Assets/Scripts/GameScript.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class GameScript : MonoBehaviour {
 
 	//public so we can access them in the editor.  easier to change for testing
-    GameObject[] field;
     public int width;
 	public int height;
     public float padding = .5f; //Distance between orbs on board
-	public int startX;
-	public int startY;
     int size;
 	public GameObject[,] allOrbs;
 
@@ -21,32 +18,48 @@ public class GameScript : MonoBehaviour {
 		size = width * height;
 
 		//makes 2d array for orbs
-		allOrbs = new GameObject[width,height];
+		allOrbs = new GameObject[height,width];
 
-		field = new GameObject[size];
-
-        int ii = startX;
-        int jj = startY;
-
-		//Fills table with random orbs
-		for(int i = 0; i < width; i++){  //		for(int i = 0 + startX; i < width + startX; i++){
-			for(int j = 0; j < height; j++){ //			for(int j = 0 + startY; j < height + startY; j++){
-				Vector2 tempPos = new Vector2 (ii, jj);
-				GameObject orb = Instantiate(Resources.Load("Prefabs/Orb"), tempPos, Quaternion.identity) as GameObject;
+        //Fills table with random orbs
+        for (int i = 0; i < height; i++){
+			for(int j = 0; j < width; j++){
+				GameObject orb = Instantiate(Resources.Load("Prefabs/Orb"), new Vector3(j, i), Quaternion.identity) as GameObject;
 	            orb.GetComponent<Orb>().type = (OrbType)Random.Range(0, 5); //Will need to change this later because pure random isn't going to get a lot of possible moves
 	            //orb.transform.position = new Vector3((-(width - 1) / 2f + i % width) * padding, i / width * padding); //Grid is centered horizontally at the position and builds upward
 				orb.name = "( " + i + ", " + j + " )";
 				allOrbs[i,j] = orb;
-                ii++;
 			}
-            ii = startX;
-            jj++;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+
+    }
+
+    public void MovePieces(int row, int column, float swipeAngle)
+    {
+        // 5            right
+        // 90           up
+        // -180         left
+        // -90          down
+
+        //right swipe
+        if (swipeAngle > -45 && swipeAngle <= 45)
+        {
+            GameObject storedOrb = allOrbs[row, width - 1];
+            for (int i = 0; i < width; i++)
+            {
+                GameObject orb = allOrbs[row, i];
+                int nextPos = (i + 1) % width;
+                //Debug.Log("if: i = " + i + "   j: " + j);
+                orb.transform.position = new Vector3(nextPos, row);
+                orb.name = "( " + row + ", " + nextPos + " )";
+                allOrbs[row, i] = storedOrb;
+                storedOrb = orb;
+            }
+        }
+        //left swipe
+    }
 }
--- GDD2UnityGame/Assets/Scripts/OrbScript.cs
+++ GDD2UnityGame/Assets/Scripts/OrbScript.cs
@@ -12,58 +12,39 @@ public class OrbScript : MonoBehaviour {
 	public int column;
 	private GameScript board;
 	private GameObject otherdot;
-	public int targetX;
-	public int targetY;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         
 		board = FindObjectOfType<GameScript> ();
-		targetX = (int)transform.position.x;
-		targetY = (int)transform.position.y;
+		row = (int)transform.position.y;
+		column = (int)transform.position.x;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		targetX = column;
-		targetY = row;
+	void Update ()
+    {
+        if(row != (int)transform.position.y) row = (int)transform.position.y;
+        if(column != (int)transform.position.x) column = (int)transform.position.x;
+    }
 
-	}
-
-	private void OnMouseDown(){
+	private void OnMouseDown()
+    {
 		firstTouchPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 
 	}
 
-	private void OnMouseUp(){
+	private void OnMouseUp()
+    {
 		finalTouchPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		CalcAngle ();
 	}
 
-	void CalcAngle(){
+	void CalcAngle()
+    {
 		swipeAngle = Mathf.Atan2 (finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x) * 180 / Mathf.PI;
-		Debug.Log (swipeAngle);
+		//Debug.Log (swipeAngle);
+        board.MovePieces(row, column, swipeAngle);
 	}
-
-	void MovePieces(){
-		//right swipe
-		if (swipeAngle > -45 && swipeAngle <= 45) {
-			for (int i = 0; i < board.width; i++) {
-				for (int j = 0; j < board.height; j++) {
-					if (j == row) {
-						//if its the last emelment
-						if(board.allOrbs[i +1,j] == null){
-							board.allOrbs [i, j] = board.allOrbs[0, j];
-							continue;
-						}
-						board.allOrbs [i, j] = board.allOrbs[i + 1, j];
-					}
-				}
-			}
-		}
-
-		//left swipe
-	}
-
-
 }
