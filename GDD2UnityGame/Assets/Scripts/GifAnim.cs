0a60e76e5a9c44ff2a08d7ba60000b04d463a7fb
Zack D <zsd7200@rit.edu>
2018-11-04 03:37:32 -0500

    Started working on how to play menu

---
+++ GDD2UnityGame/Assets/Scripts/GifAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GifAnim : MonoBehaviour
+{
+
+    public Texture2D sheet;
+    Sprite[] frames;
+    int fps = 33;
+
+    void Start()
+    {
+        frames = Resources.LoadAll<Sprite>("Spritesheets\\" + sheet.name);
+        Debug.Log(sheet.name);
+        Debug.Log(frames.Length);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        int index = (int)((Time.time * fps) % frames.Length);
+        GetComponent<SpriteRenderer>().sprite = frames[index];
+    }
+}
