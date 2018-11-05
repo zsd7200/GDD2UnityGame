88fdd601031f3a89f3f3088b516d070c3dbd2f02
Zack Dunham (RIT Student) <zsd7200@ad.rit.edu>
2018-11-04 22:52:26 -0500

    Level select is done yeet

--- GDD2UnityGame/Assets/Scripts/LevelSelectScript.cs
+++ GDD2UnityGame/Assets/Scripts/LevelSelectScript.cs
@@ -5,8 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelectScript : MonoBehaviour
 {
+    public int level;
+
     public void Back()
     {
         SceneManager.LoadScene("Menu");
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material = Resources.Load<Material>("Materials\\ButtonBackdrop_Hover");
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = Resources.Load<Material>("Materials\\TextBackdrop");
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Renderer>().material = Resources.Load<Material>("Materials\\BoardBackdrop");
+    }
+
+    private void OnMouseUp()
+    {
+        SceneManager.LoadScene("Level" + level);
     }
 }
